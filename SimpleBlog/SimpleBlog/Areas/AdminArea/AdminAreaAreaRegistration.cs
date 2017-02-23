1270b9cb18b5b263a043d73a8be1d96757c799d8
Cem Çakmakcı <cem_cakmakci@hotmail.com>
2017-02-23 17:56:32 +0200

    commit

---
+++ SimpleBlog/SimpleBlog/Areas/AdminArea/AdminAreaAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SimpleBlog.Areas.AdminArea
+{
+    public class AdminAreaAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "adminArea";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "adminArea_default",
+                "adminArea/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ SimpleBlog/SimpleBlog/Areas/AdminArea/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog.Areas.AdminArea.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: AdminArea/Users
+        public ActionResult Index()
+        {
+            return Content("Users");
+        }
+        public ActionResult Edit()
+        {
+            return Content("Edit");
+        }
+        public ActionResult List()
+        {
+            return Content("List");
+        }
+    }
+}
